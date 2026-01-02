7fedebb9129f6498ea878297ea65aa94ae11c86e
RushourZZ <131259837+RushourZZ@users.noreply.github.com>
2026-01-02 11:49:24 +0100

    added js

--- script.js
+++ script.js
@@ -13,32 +13,66 @@ const popupImages = [
     "./img/wolken.png",
 ];
 
+let currentIndex = 0;
 
 // * Öffnet den Dialog (Popup)
-function openDialog(image) {
+function openDialog(index) {
     const dialogRef = document.getElementById("openPicture");
     dialogRef.showModal();
-    const numbering = image.getAttribute("pictureNumber");
-    counter(numbering);
-     
+    currentIndex = index;
+    render();
+
+    counter();
 }
 // * Der Zähler der Bilder wird aus dem HTML picture Number übernommen -> wurde in openDialog definiert.
-function counter(imageCounter) {
-     
-    console.log(imageCounter);
-    
-
+function counter() {
+    console.log(currentIndex);
 }
 
 // * schließ das Popup
 function closeDialog() {
     const dialogRef = document.getElementById("openPicture");
     dialogRef.close();
-    
 }
 // *Sobald das Popup geöffnet ist soll anhand des Indexes das richtige Bild angezeigt werden..
-function showPictures (){
-    if(openDialog == true){
-        
-    }
+
+// ! Diese Funktion leert den Dialog Bereich und bereitet diesen auf innerHtml vor
+
+function render() {
+    let pictureRef = document.getElementById("bigPicture");
+
+    pictureRef.src = popupImages[currentIndex];
+
+    setPictureName();
+    setNumber();
 }
+
+function setPictureName() {
+    let nameRef = document.getElementById("fileName");
+
+    let imageName = popupImages[currentIndex].replace("./img/", "");
+    nameRef.innerText = imageName;
+}
+
+function next() {
+    currentIndex++;
+    if (currentIndex >= popupImages.length) {
+        currentIndex = 0;
+    }
+    render();
+}
+
+function previous() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = popupImages.length - 1;
+    }
+    render();
+}
+
+function setNumber() {
+    let numberRef = document.getElementById("navNumbering");
+    numberRef.innerText = currentIndex + 1 + "/" + popupImages.length;
+}
+
+
